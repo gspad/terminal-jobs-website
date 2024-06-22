04b42cfc5ebe03f776a1a9815d1e51a5a3980950
Gian Luca Spadafora <gianlspadafora@gmail.com>
2024-06-22 22:18:09 +0200

    AAARRRGGGG

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,7 +31,7 @@
 	onMount(typeWriter1);
 </script>
 
-<div class="container h-full mx-auto flex justify-center mt-80">
+<div class="container h-full mx-auto flex justify-center mt-20 lg:mt-80">
 	<div class="space-y-36 items-center text-center">
 		<div class="text-4xl lg:text-6xl text-white">opportunities for top engineers.</div>
 
