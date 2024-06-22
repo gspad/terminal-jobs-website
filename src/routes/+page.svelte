df6d9540ebd900ec35245960a4120659facd344e
Gian Luca Spadafora <gianlspadafora@gmail.com>
2024-06-22 18:19:33 +0200

    Make text expand only from left

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,8 @@
 	import { onMount } from 'svelte';
 
 	let initialCommand = 'npx terminaljobs --help';
-	let initialCommand2 =
-		'npx terminaljobs --level=senior title=devops --tech=aws,kubernetes,terraform';
+	let initialCommand2 = 'npx terminaljobs --level=senior title=devops';
 
-	let command = 'npx terminaljobs --location=remote';
-	let command2 = 'npx terminaljobs --help';
-	let command3 = 'npx terminaljobs --lang=typescript --location=on-site';
-	let command4 = 'npx terminaljobs --level=junior --title=devops --remote=false';
 	let displayedCommand = '$ ';
 	let displayedCommand2 = '$ ';
 	let i = 0;
@@ -33,26 +28,18 @@
 	};
 
 	onMount(typeWriter1);
-
-	function copyToClipboard() {
-		navigator.clipboard.writeText(command);
-	}
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-36 text-center flex flex-col items-center">
+	<div class="space-y-36 items-center text-center">
 		<div class="text-6xl text-white">opportunities for top engineers.</div>
 
-		<div class="typewriters space-y-10">
-			<div class="card-wrapper">
-				<div class="card-content text-2xl text-gray-300">
-					{displayedCommand}
-				</div>
+		<div class="typewriters space-y-10 text-center">
+			<div class="typewriter first-command centered text-2xl text-gray-300">
+				<span class="inline-block text-left w-full">{displayedCommand}</span>
 			</div>
-			<div class="card-wrapper">
-				<div class="card-content text-2xl text-gray-300">
-					{displayedCommand2}
-				</div>
+			<div class="typewriter second-command text-2xl text-gray-300">
+				<span class="inline-block text-left w-full">{displayedCommand2}</span>
 			</div>
 		</div>
 	</div>
@@ -71,12 +58,6 @@
 		}
 	}
 
-	.card-wrapper {
-		position: relative;
-		padding: 0.25rem; /* Padding to make space for the border */
-		border-radius: 1rem;
-	}
-
 	.button {
 		border-radius: 1rem;
 	}
@@ -84,5 +65,39 @@
 	.white {
 		background: rgb(255, 255, 255);
 		font-weight: bold;
+	}
+
+	.typewriters {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.card-content {
+		display: flex;
+		justify-content: center;
+		left: 50%;
+	}
+
+	.typewriter {
+		font-family: monospace;
+		overflow: hidden;
+		/* Ensures the content is not revealed until the animation */
+		border-right: 0.15em solid orange;
+		/* The typwriter cursor */
+		white-space: nowrap;
+		/* Keeps the content on a single line */
+		margin-left: 0;
+		margin-right: auto;
+		/* Gives that scrolling effect as the typing happens */
+		letter-spacing: 0.15em;
+	}
+
+	.first-command {
+		margin-left: 25%;
+	}
+
+	.second-command {
+		margin-left: 3%;
 	}
 </style>
